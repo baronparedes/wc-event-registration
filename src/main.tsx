d195ac7da97778a940d710a1c43ca876579406bb
baronparedes <baronpatrick.paredes@gmail.com>
2026-08-12 00:38:57 +0800

    feat: conditionally inject Vercel analytics script in production

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-inject();
+if (import.meta.env.PROD) {
+  inject({
+    scriptSrc: 'https://va.vercel-scripts.com/v1/script.js',
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
