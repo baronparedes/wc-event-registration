3e14b9556044c5f729a873f39c93393d0afaae7c
Baron Paredes <baronpatrick.paredes@gmail.com>
2026-09-06 20:49:56 +0800

    Add public registration option to member lookup and update tests

--- src/pages/events/[slug]/register/components/MemberLookupStepCard.tsx
+++ src/pages/events/[slug]/register/components/MemberLookupStepCard.tsx
@@ -74,6 +74,8 @@ export function MemberLookupStepCard(props: MemberLookupStepCardProps) {
             allowNameLookup={allowNameLookup}
             isLookupPending={isLookupPending}
             onSelectMethod={setLookupMethod}
+            allowPublicRegistration={allowPublicRegistration}
+            slug={slug}
           />
         )}
 
@@ -187,18 +189,6 @@ export function MemberLookupStepCard(props: MemberLookupStepCardProps) {
 
         {/* Guest/non-member registration option */}
       </WizardStep>
-
-      {slug && allowPublicRegistration && (
-        <div className="flex items-center justify-center pt-4">
-          <Button
-            type="button"
-            onClick={() => navigate(`/events/${slug}/register-public`)}
-            className="text-xl transition hover:text-primary/80 w-full"
-          >
-            Join the event as a Guest
-          </Button>
-        </div>
-      )}
     </>
   );
 }
