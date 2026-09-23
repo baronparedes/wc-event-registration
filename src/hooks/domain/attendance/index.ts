4977253c6633eaa6caffee32953b4006fe68dca8
baronparedes <1380651+baronparedes@users.noreply.github.com>
2026-09-23 05:14:19 +0000

    refactor: replace wildcard exports in attendance domain with explicit named exports

--- src/hooks/domain/attendance/index.ts
+++ src/hooks/domain/attendance/index.ts
@@ -1,3 +1,27 @@
-export * from './queries';
-export * from './mutations';
-export * from './state';
+// Queries
+export { useAttendanceSettingsQuery } from './queries';
+export { useSearchAttendeesQuery } from './queries';
+export { useAttendeesLocalCacheQuery, searchAttendeesLocally } from './queries';
+export { useAttendanceSlotSummariesQuery } from './queries';
+export { useAttendanceUnregisteredMembersQuery } from './queries';
+export { useAttendanceSavedViewsQuery } from './queries';
+export { useAttendanceSavedViewQuery } from './queries';
+
+// Mutations
+export { useUpdateAttendanceSettingsMutation } from './mutations';
+export { useUpsertAttendanceAnswersMutation } from './mutations';
+export { useCheckInAttendeeMutation } from './mutations';
+export { useQueuedCheckInAttendeeMutation } from './mutations';
+export { useDownloadAttendanceCSVMutation } from './mutations';
+export { useExportAttendanceCSVMutation } from './mutations';
+export { useExportUnregisteredMembersCSVMutation } from './mutations';
+export { useBulkUpsertAttendanceAnswersMutation } from './mutations';
+export { useUpsertAttendanceSavedViewMutation } from './mutations';
+export { useDeleteAttendanceSavedViewMutation } from './mutations';
+
+// State
+export { useAttendanceViewControlsState } from './state';
+export { useAttendanceCheckInRealtime } from './state';
+export { useAttendanceSlotRecordRealtime } from './state';
+export { useOfflineAttendanceDataSnapshot } from './state';
+export { useOfflineCheckInEventSettings } from './state';
