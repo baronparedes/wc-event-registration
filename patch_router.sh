058021b0a5ad0f714e1ff8241eec86b94f6bdf01
Baron Paredes <baronpatrick.paredes@gmail.com>
2026-09-26 02:23:05 +0800

    chore: remove temporary router and routes patch scripts

--- patch_router.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Adding EventCountdownPage import
-sed -i "s/const PublicEventRegistrationPage = lazy(() =>/const EventCountdownPage = lazy(() =>\n  import('..\/pages\/events\/[slug]\/countdown').then((module) => ({\n    default: module.EventCountdownPage,\n  })),\n);\nconst PublicEventRegistrationPage = lazy(() =>/g" src/app/router.tsx
-
-# Adding to routeComponents
-sed -i "s/  eventPublicRegister: PublicEventRegistrationPage,/  eventPublicRegister: PublicEventRegistrationPage,\n  eventCountdown: EventCountdownPage,/g" src/app/router.tsx
